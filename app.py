bc5d997cd1696cf1c225daac4a0c6a84c6868c7a
Sebastian Farias <sfarias@gmail.com>
2025-10-14 13:00:31 -0300

    Agrega lógica de indicadores para dashboard

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 # app.py
 
 import streamlit as st
-from indicadores import brechas_ingresos, dependencia
+from indicadores import brechas_ingresos, dependencia, ingresos
 
 st.set_page_config(layout="wide", page_title="Dashboard de Indicadores Regionales")
 
 st.sidebar.title("📊 Panel de Indicadores")
 opcion = st.sidebar.radio(
     "Selecciona un indicador:",
-    ["Brechas de Ingresos", "Dependencia"]
+    ["Brechas de Ingresos", "Dependencia", "Ingresos"]
 )
 
 DASHBOARDS = {
     "Brechas de Ingresos": brechas_ingresos,
-    "Dependencia": dependencia
+    "Dependencia": dependencia,
+    "Ingresos": ingresos
 }
 
 if opcion in DASHBOARDS:
